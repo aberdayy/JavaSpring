5fd6bccf0060d5824cb94d196191647e64d3dd22
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-03-31 16:00:37 +0300

    String Value Injection

--- SpringIocAndDependencyInjection/src/SpringIntro/msSqlCustomerDal.java
+++ SpringIocAndDependencyInjection/src/SpringIntro/msSqlCustomerDal.java
@@ -1,9 +1,16 @@
 package SpringIntro;
 
 public class msSqlCustomerDal implements ICustomerDal{
-
+	String connectionString;
+	public String getConnectionString() {
+		return connectionString;
+	}
+	public void setConnectionString(String connectionString) {
+		this.connectionString = connectionString;
+	}
 	@Override
 	public void add() {
+		System.out.println("Connection String : " +this.connectionString);
 		System.out.println("MsSQL veritabania gecildi");
 		
 	}
