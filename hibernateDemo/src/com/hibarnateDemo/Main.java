05302fd7096d204e60fe0718f8e1e3e80efea002
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-04-06 00:40:08 +0300

    CRUD Operations on Hibernate

--- hibernateDemo/src/com/hibarnateDemo/Main.java
+++ hibernateDemo/src/com/hibarnateDemo/Main.java
@@ -22,13 +22,35 @@ public class Main {
 			//FROM City c WHERE c.name LIKE '%kar%' 
 			//FROM City c ORDER BY c.name DESC 
 			//List<City> cities = session.createQuery("select c.countryCode from City c GROUP BY c.countryCode ").getResultList();
-			List<String> countryCodes = session.createQuery("select c.countryCode from City c GROUP BY c.countryCode ").getResultList();
- 			
-			for(String countryCode:countryCodes) {
-				System.out.println(countryCode);
-			}
 			
+			/*
+			 * List<String> countryCodes = session.
+			 * createQuery("select c.countryCode from City c GROUP BY c.countryCode ").
+			 * getResultList();
+			 * 
+			 * for(String countryCode:countryCodes) { System.out.println(countryCode); }
+			 */
+			//INSERT
+//			City city = new City();
+//			city.setName("Istanbul 10");
+//			city.setCountryCode("TUR");
+//			city.setDistrict("Marmara");
+//			city.setPopulation(100);
+//			session.save(city);
+			
+			//UPDATE
+			/*
+			 * City city = session.get(City.class, 1000); city.setPopulation(10);
+			 * session.save(city);
+			 * 
+			 * System.out.println(city.getName());
+			 */
+			//DELETE
+			City city = session.get(City.class, 1000);
+			session.delete(city);
 			session.getTransaction().commit();
+	
+			
 		}finally{
 			factory.close();
 		}
