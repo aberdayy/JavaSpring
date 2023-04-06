fcebe3e07f677a84b0cac5e336b9950736c0f57a
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-04-06 20:28:10 +0300

    Working with Maven

---
+++ MavenIntro/src/main/java/com/abeTech/intro/App.java
@@ -0,0 +1,13 @@
+package com.abeTech.intro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
