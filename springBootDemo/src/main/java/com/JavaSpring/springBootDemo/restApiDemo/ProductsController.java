fe84dd9a199bd3447fbf500c5912b2df235b03e1
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-04-07 15:45:35 +0300

    First Spring Boot REST apptication

--- springBootDemo/src/main/java/com/JavaSpring/springBootDemo/restApiDemo/ProductsController.java
+++ springBootDemo/src/main/java/com/JavaSpring/springBootDemo/restApiDemo/ProductsController.java
@@ -1,5 +1,19 @@
 package com.JavaSpring.springBootDemo.restApiDemo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductsController {
+	@GetMapping("/")
+	public String get() {
+		return "Laptop";
+		
+	}
+	@GetMapping("/products")
+	public String get2() {
+		return "Laptop 2";
+		
+	}
 	
 }
