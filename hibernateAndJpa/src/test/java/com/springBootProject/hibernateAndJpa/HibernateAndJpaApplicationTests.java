2b94bf9cf4109af647b6c2082ba299ae14ba4530
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-04-07 16:16:43 +0300

    Hibernate and JPA Entegration | Apptication Properties | Entity package and Usage

---
+++ hibernateAndJpa/src/test/java/com/springBootProject/hibernateAndJpa/HibernateAndJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootProject.hibernateAndJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateAndJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
